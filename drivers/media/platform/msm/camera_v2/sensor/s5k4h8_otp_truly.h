b2f1cbd59aa226330a9dc7304fccec6791ff317f
Palettaemir <101826996+Palettaemir@users.noreply.github.com>
2022-08-17 22:11:31 +0200

    Cm-13.0

--- drivers/media/platform/msm/camera_v2/sensor/s5k4h8_otp_truly.h
+++
@@ -1,7 +0,0 @@
-#ifndef S5K4H8_OTP_H
-#define S5K4H8_OTP_H
-
-int32_t s5k4h8_otp_config(struct msm_sensor_ctrl_t *s_ctrl);
-uint16_t s5k4h8_otp_get_mid(struct msm_sensor_ctrl_t *s_ctrl);
-
-#endif //S5K4H8_OTP_H
